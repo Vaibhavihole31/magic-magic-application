19075303fbd03fec4d617220c23378035e98a087
Vaibhavihole31 <itspinki05@gmail.com>
2025-06-20 19:23:15 +0530

    new message added

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 function App() {
   return (
-    <h1>Magic magic Application</h1>
+   <>
+   <h1>Hello Students,  Good Evening</h1>
+    <h1>Magic Magic Application</h1>
+   </>
   )
 }
 
